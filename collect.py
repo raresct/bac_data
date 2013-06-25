36c0c9e67927b8a3080de36f24da1f8ece9b2e2d
Rares Turliuc <sufletul_din_vis@yahoo.com>
2013-06-25 10:49:28 +0100

    init

--- collect.py
+++ collect.py
@@ -1,54 +1,75 @@
-from urllib2 import urlopen
-import re
-import pyodbc
-from BeautifulSoup import BeautifulSoup
+#!/usr/bin/env python
+
+from selenium import webdriver
 
 def clean_var (var):
+  if var is None:
+   return None
+  else:
     return var.strip().lower()
 
+def collect(year):
+  year = str(year)
+  with open('out.txt', 'a') as f:
+    header = ('Nume complet', 'Judet', 'Pozitie tara', 'Unitate invatamant', 'Promotie anterioara', # 4
+        'Forma invatamant', 'Specializare', 'Nota Limba romana (1)', 'Nota Limba romana (2)', 'Disciplina obligatorie', # 9
+        'Nota la disciplina obligatorie (1)', 'Nota la disciplina obligatorie (2)', 'Disciplina la alegere',
+        'Nota la disciplina la alegere (1)', 'Nota la disciplina la alegere (2)', # 14
+        'Media', 'Admis'
+        )
+    len_header = len(header)
+    #f.write('|'.join(header)+'\n')
 
-url = 'http://examene.calificativ.ro/examene/rezultate-bacalaureat/2012/'
-page = urlopen(url).read()
-areas = []
-
-# Read district names
-for val in re.sub('<(.*?)>','',page).split('\n'):
-    if len(val.split('/')[0].strip()) < 3 and len(val.split('/')[0].strip()) > 0:
-        areas.append(val.split('/')[0])
-
-for area in areas:
     # Read html filenames
-    html = []
-    url = 'http://examene.calificativ.ro/examene/rezultate-bacalaureat/2012/'+area+'/'
-    page = urlopen(url).read()
+    base_url = 'http://static.bacalaureat.edu.ro/'+year+'/rapoarte/rezultate/dupa_medie/'
+    page_no = 20020
+    driver = webdriver.Firefox()
+    driver.implicitly_wait(10)
 
-    for val in re.sub('<(.*?)>',' ',page).split('\n'):
-        if re.match('.*html', val.strip().split(' ')[0]):
-            html.append(val.strip().split(' ')[0])
-    
-    for file in html:
+    while True:
+      print page_no
+      url = base_url+'page_{}.html'.format(page_no)
+      driver.get(url)
+      if '404' in driver.title:
+        return
 
-        url = 'http://examene.calificativ.ro/examene/rezultate-bacalaureat/2012/'+area+'/'+file
-        page = urlopen(url).read()
+      # surround in try except until it can read
+      table = driver.find_element_by_id('mainTable')
 
-        soup = BeautifulSoup(page)
-        table = soup.find('table')
-         
-        rows = table.findAll('tr')
-        i = -1
-        list = []
-        for tr in rows:
-            i += 1
-            if i == 2:
-                i = 0
-                print '|'.join(list)
-                list = []
-            cols = tr.findAll('td')
-            for td in cols:
-                if len(td.contents) > 0:
-                    if len(td.contents) > 2:
-                        list.append(clean_var(td.contents[0] + td.contents[2]))
-                    else:
-                        list.append(clean_var(td.contents[0]))
-                else:
-                    list.append('None')
+      rows =  table.text.split('\n')
+
+      csv_row = [None for i in xrange(len_header)]
+
+
+      for i in xrange(2, len(rows), 3): # first two rows are irrelevant, and we read three at a time
+        row1 = clean_var(rows[i]).split('  ')
+        row2 = clean_var(rows[i+1]).split('  ')
+        row3 = clean_var(rows[i+2]).split('  ')
+
+        csv_row[0] = row1[1]+' '+row2[0] # nume
+        csv_row[1] = row2[2] # judet
+        csv_row[2] = row1[0] # poz tara
+        csv_row[3] = row2[1] # unit inv
+        csv_row[4] = u'0' if row2[3]=='nu' else u'1' # prom ant
+        csv_row[5] = row2[4] # forma_inv
+        csv_row[6] = row2[5] # spec
+        csv_row[7] = row2[7] # nota lb rom (1)
+        csv_row[8] = row2[9] # nota lb rom (2)
+        csv_row[9] = row2[13].strip() # disciplina obligatorie
+        csv_row[12] = row2[14] # disc alegere
+        csv_row[10] = row3[0] # nota disc oblig (1)
+        csv_row[11] = row3[1] # nota disc oblig (2)
+        csv_row[13] = row3[2] # nota disc alegere (1)
+        csv_row[14] = row3[3] # nota disc alegere (2)
+        csv_row[15] = row2[16] # media
+        csv_row[16] = u'1' if row2[17]==u'reu\u015fit' else u'0' # admis
+
+        f.write(('|'.join(csv_row)+'\n').encode('utf-8'))
+      page_no += 1
+    driver.close()
+def main():
+  collect(2012)
+
+if __name__=='__main__':
+  main()
+
---
+++ process_data.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import numpy as np
+import Image
+import ImageDraw
+import ImageFont
+
+def black_or_white(val):
+  val = np.array(val)
+  black = np.array([0,0,0])
+  white = np.array([255,255,255])
+  dist_black = np.linalg.norm(val-black)
+  dist_white = np.linalg.norm(val-white)
+  if dist_black < dist_white:
+    return tuple(black)
+  else:
+    return tuple(white)
+
+def transform_bw(img):
+  #  cast image to either black or white
+  new_img = [ black_or_white(val)  for val in img.getdata()]
+  img.putdata(new_img)
+  img.save("a_test.png")
+
+def load_data():
+  data = pd.read_csv('bac2012.csv', sep='|', header=0,
+      names = ['name', 'county', 'pos', 'unit', 'retry', 'type', 'special',
+        'ro1', 'ro2', 'oblig', 'oblig1', 'oblig2',
+        'choice', 'choice1', 'choice2', 'avg', 'admitted'])
+  admit = data[data['admitted'] >0]
+  print len(admit)
+  admit_county = data[['county', 'admitted']]
+  grouped = admit_county.groupby('county')
+  county_percents = grouped.aggregate(np.average)
+  print county_percents.sort('admitted')
+  harta_jud = Image.open('docs/harta_bw.png')
+  #harta_jud = harta_jud.convert('1')
+  county_pos = {
+    'ab' : (140, 130),
+    'ag' : (210, 210),
+    'ar' : (100, 130),
+    'b' : (273, 243), # + if
+    'bc' : (300, 140),
+    'bh' : (100, 100),
+    'bn' : (200, 60),
+    'br' : (340, 200),
+    'bt' : (300, 20),
+    'bv' : (230, 160),
+    'bz' : (300, 220),
+    'cj' : (170, 100),
+    'cl' : (330, 270),
+    'cs' : (90, 200),
+    'ct' : (380, 260),
+    'cv' : (270, 170),
+    'db' : (240, 220),
+    'dj' : (160, 260),
+    'gj' : (150, 210),
+    'gl' : (350, 160),
+    'gr' : (260, 270),
+    'hd' : (120, 160),
+    'hr' : (240, 100),
+    'il' : (340, 240),
+    'is' : (300, 70),
+    'mh' : (110, 230),
+    'mm' : (200, 40),
+    'ms' : (220, 100),
+    'nt' : (280, 80),
+    'ot' : (200, 260),
+    'ph' : (260, 200),
+    'sb' : (190, 150),
+    'sj' : (150, 70),
+    'sm' : (140, 30),
+    'sv' : (250, 40),
+    'tl' : (410, 220),
+    'tm' : (70, 170),
+    'tr' : (230, 270),
+    'vl' : (180, 200),
+    'vn' : (300, 160),
+    'vs' : (340, 110)
+  }
+  print float(county_percents.loc['ab'])
+  draw = ImageDraw.Draw(harta_jud)
+  font = ImageFont.truetype("/Library/Fonts/Georgia.ttf",12)
+  for k,v in county_pos.items():
+    (x,y) = v
+    alpha = county_percents.loc[k]
+    ImageDraw.floodfill(harta_jud, (x,y), (int((1-alpha)*255), 0, int(alpha*255)))
+    draw.text((x,y), k, (0, 0, 0), font=font)
+  harta_jud.save("a_test.png")
+
+
+
+def main():
+  load_data()
+
+if __name__=='__main__':
+  main()
