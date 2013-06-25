fbec4c19a5679fd97ef66fff1c82b59b274ce1cc
Rares Turliuc <sufletul_din_vis@yahoo.com>
2013-06-25 19:00:34 +0100

    better plot

--- process_data.py
+++ process_data.py
@@ -21,7 +21,7 @@ def transform_bw(img):
   #  cast image to either black or white
   new_img = [ black_or_white(val)  for val in img.getdata()]
   img.putdata(new_img)
-  img.save("a_test.png")
+  img.save("img/a_test.png")
 
 def load_data():
   data = pd.read_csv('bac2012.csv', sep='|', header=0,
@@ -33,8 +33,10 @@ def load_data():
   admit_county = data[['county', 'admitted']]
   grouped = admit_county.groupby('county')
   county_percents = grouped.aggregate(np.average)
-  print county_percents.sort('admitted')
-  harta_jud = Image.open('docs/harta_bw.png')
+  county_percents = county_percents.sort('admitted')
+  print county_percents
+  harta_jud = Image.open('img/harta_bw3.png')
+  #transform_bw(harta_jud)
   #harta_jud = harta_jud.convert('1')
   county_pos = {
     'ab' : (140, 130),
@@ -80,14 +82,17 @@ def load_data():
     'vs' : (340, 110)
   }
   print float(county_percents.loc['ab'])
+  percent_min =  float(county_percents.iloc[0])
+  percent_max = float(county_percents.iloc[-1])
+  percent_delta = percent_max-percent_min
   draw = ImageDraw.Draw(harta_jud)
   font = ImageFont.truetype("/Library/Fonts/Georgia.ttf",12)
   for k,v in county_pos.items():
     (x,y) = v
     alpha = county_percents.loc[k]
-    ImageDraw.floodfill(harta_jud, (x,y), (int((1-alpha)*255), 0, int(alpha*255)))
+    ImageDraw.floodfill(harta_jud, (x,y), (int((1-alpha)*255), int(alpha*255), 0))
     draw.text((x,y), k, (0, 0, 0), font=font)
-  harta_jud.save("a_test.png")
+  harta_jud.save("img/a_test.png")
 
 
 
